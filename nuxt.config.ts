0e0fe1aa305121e4d4d3c141cbe33d549fe69cb4
Alexis Lopes <alexisdrops@gmail.com>
2023-06-29 21:03:13 -0300

    prepare for deploy 🚀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/alexisme/"
+  },
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss', 'nuxt-icon'
   ]
 })
