9cb23a57de3ecd5300e9eb1bb62145871d523d75
Alexis Lopes <alexisdrops@gmail.com>
2023-10-21 21:28:54 -0300

    Updates

--- _nuxt/config.61071288.js
+++
@@ -1,1 +0,0 @@
-import{H as p,j as o,I as i}from"./entry.6767972b.js";const f={},t=p(f);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
---
+++ _nuxt/config.a8ba3410.js
@@ -0,0 +1,1 @@
+import{H as p,j as o,I as i}from"./entry.bfa8d54c.js";const f={},t=p(f);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
