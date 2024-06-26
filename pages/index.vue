54d96c126e390d90bb866f3927bf41e7868bcd09
Alexis Lopes <alexisdrops@gmail.com>
2024-06-25 22:20:46 -0300

    remove redirect ➡️

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script lang="ts" setup>
+
 const config = useRuntimeConfig()
 
 onMounted(() => {
-  navigateTo(`https://accounts.spotify.com/authorize?response_type=code&client_id=${config.public.spotifyClientId}&scope=${config.public.spotifyApiScope}&redirect_uri=http://${document.location.host}/home&state=${config.public.spotifyApiState}`, { external: true })
+  navigateTo('/home')
+  // navigateTo(`https://accounts.spotify.com/authorize?response_type=code&client_id=${config.public.spotifyClientId}&scope=${config.public.spotifyApiScope}&redirect_uri=http://${document.location.host}/home&state=${config.public.spotifyApiState}`, { external: true })
 })
 </script>
 
