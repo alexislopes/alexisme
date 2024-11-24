f3181cba2f88117f526f431f389f91b4cc3d16e0
Alexis Lopes <alexisdrops@gmail.com>
2024-11-24 20:59:20 -0300

    adjust position of nav  options 🧭

--- components/Nav.vue
+++ components/Nav.vue
@@ -7,7 +7,7 @@
       </span>
       <Icon v-show="!opened" @click="opened = true" name="solar:hamburger-menu-bold" class="lg:hidden text-xl" />
       <div :class="!opened ? '-left-2/3' : 'left-0 rounded-r-md'"
-        class="peer-focus:left-0 ease-out delay-150 duration-300 flex gap-4 absolute top-0 bottom-0 w-2/3 flex-col h-screen p-4 bg-[#fefefe] z-20 lg:bg-transparent lg:relative lg:w-auto lg:h-auto lg:flex-row lg:p-0">
+        class="ease-out delay-150 duration-300 flex gap-4 absolute top-0 bottom-0 w-2/3 flex-col h-screen p-4 bg-[#fefefe] z-20 lg:bg-transparent lg:relative lg:w-auto lg:h-auto lg:flex-row lg:p-0 lg:left-0">
         <div class="flex justify-end lg:hidden">
           <Icon @click="opened = false" name="ic:round-close" />
         </div>
